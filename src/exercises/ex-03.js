f50f38ba3b1640234aff00cd296ad2fc29ff5971
m.zmmrl <e273626@edu.sbl.ch>
2026-01-16 13:42:57 +0100

    fertigstellung ex2 + 3

---
+++ src/exercises/ex-03.js
@@ -0,0 +1,5 @@
+const myname = process.argv[2]
+const myage = Number(process.argv[3])
+const maturage = myage + 4
+
+console.log(`Hallo ${myname} dein alter ist ${myage}, an der Matur wirst du ${maturage} jahre alt sein.`)
